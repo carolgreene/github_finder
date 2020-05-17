c2c7a136882f4da81c83f031b00c254cca81a120
carolgreene <carolgreene3@gmail.com>
2020-05-17 13:55:22 -0700

    added alert & cleared data

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //Init Github
 const github = new Github
 
+//Init UI
+const ui = new UI;
+
 //Search input
 const searchUser = document.getElementById('searchUser')
 
@@ -14,15 +17,13 @@ searchUser.addEventListener('keyup', (e) => {
     github.getUser(userText)
     .then(data => {
       if(data.profile.message === 'Not Found') {
-        // show alert
+        ui.showAlert('User not found', 'alert alert-danger');
 
       } else {
-        //show profile
-
+        ui.showProfile(data.profile)
       }    
     })    
   } else {
-    //clear profile
-    
+    ui.clearProfile()
   }
 })
--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ class UI {
     this.profile = document.getElementById('profile')
   }
 
+  //display profile
   showProfile(user) {
     this.profile.innerHTML = `
     <div class="card card-body mb-3">
@@ -30,4 +31,40 @@ class UI {
     <div id="repos"></div>
     `;
   }
+
+  //show alert
+  showAlert(message, className) {
+    //clear any remaining alerts
+    this.clearAlert()
+    //create a div
+    const div = document.createElement('div');
+    //add class
+    div.className = className;
+    //add text
+    div.appendChild(document.createTextNode(message))
+    //get parent
+    const container = document.querySelector('.searchContainer');
+    //get search box
+    const search = document.querySelector('.search');
+    container.insertBefore(div, search);
+
+    //Timeout after 3 secs
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  } 
+    
+  //clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  //clear profile
+  clearProfile() {
+    this.profile.innerHTML = '';
+  }
+  
 }
